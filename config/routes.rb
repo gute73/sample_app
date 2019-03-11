d2d7a6e09c560eaa77e211828598dc067e39c341
Mark D. King <kingmarkd@gmail.com>
2019-03-11 16:00:51 -0400

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
 	root 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
@@ -22,12 +18,18 @@ Rails.application.routes.draw do
 
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :account_activations, only: [:edit]
 
   resources :password_resets, only: [:new, :create, :edit, :update]
 
   resources :microposts, only: [:create, :destroy]
+
+  resources :relationships, only: [:create, :destroy]
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -19,6 +19,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", user_path(@user), count: 0
 		assert_select "a[href=?]", edit_user_path(@user), count: 0
 		assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", followers_user_path(@user), count: 0
+    assert_select "a[href=?]", following_user_path(@user), count: 0
 		get contact_path
 		assert_select "title", full_title("Contact")
 		get signup_path
@@ -37,5 +39,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", about_path
 		assert_select "a[href=?]", contact_path
 		assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", followers_user_path(@user), count: 1
+    assert_select "a[href=?]", following_user_path(@user), count: 1
 	end
 end
